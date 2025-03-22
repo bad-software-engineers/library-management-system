f4f78967663c0437196faba3d99b8a9c9a139114
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-03-22 12:17:40 +0530

    Fixes

--- db/crud.ts
+++ db/crud.ts
@@ -1,36 +1,22 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
-import { usersTable } from "./schema";
+import { verifyPending } from "./schema";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: "Dishant",
-    age: 30,
+  const user: typeof verifyPending.$inferInsert = {
+    clerkId: "Dishant",
     email: "dishant@example.com",
   };
 
   // Insert User
-  await db.insert(usersTable).values(user);
+  await db.insert(verifyPending).values(user);
   console.log("New user created!");
 
   // Retrieve Users
-  const users = await db.select().from(usersTable);
+  const users = await db.select().from(verifyPending);
   console.log("Getting all users from the database: ", users);
-
-  // Update Users
-  // await db
-  //   .update(usersTable)
-  //   .set({
-  //     age: 31,
-  //   })
-  //   .where(eq(usersTable.email, user.email));
-  // console.log("User info updated!");
-
-  // Delete
-  // await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  // console.log("User deleted!");
 }
 
 main();
