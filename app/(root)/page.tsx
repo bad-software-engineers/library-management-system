bf5bebbca1fdb103fe688b2549a5009aa8d24d7c
MeetParmar2302 <meet.parmar.2302@gmail.com>
2025-03-26 13:20:09 +0530

    Created Sarch Bar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 import BookCard from "@/components/ui/BookCard";
 import BookOverview from "@/components/ui/BookOverview";
+import SearchBar from "@/components/ui/searchBar";
 import { sampleBooks } from "@/constants";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className=" h-full flex flex-col items-center w-full">
-        <BookOverview {...sampleBooks[0]}/>
+        {/* <BookOverview {...sampleBooks[0]}/> */}
+        <div className="h-[100px] w-full flex flex-col justify-center items-center mx-5">
+          <h1 className="text-3xl font-bold">Library Management System</h1>
+          <div className="my-5">
+            <SearchBar/>
+          </div>
+        </div>
         <div className="flex flex-wrap justify-around px-10 w-full">
           {sampleBooks.map((book) => (
             <Link key={book.id} href={`/book/${book.id}`} passHref>
