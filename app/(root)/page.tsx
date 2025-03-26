40f5b493378c14558abf642f45fa0e8a3a679d7c
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-03-26 13:13:07 +0530

    Mailing Workflow with SMTP Provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,19 +3,19 @@ import BookCard from "@/components/ui/BookCard";
 import BookOverview from "@/components/ui/BookOverview";
 import { sampleBooks } from "@/constants";
 import Link from "next/link";
+import mail from "@/lib/mail";
 
 export default function Home() {
   return (
     <div className=" h-full flex flex-col items-center w-full">
-        <BookOverview {...sampleBooks[0]}/>
-        <div className="flex flex-wrap justify-around px-10 w-full">
-          {sampleBooks.map((book) => (
-            <Link key={book.id} href={`/book/${book.id}`} passHref>
-              <BookCard {...book} />
-            </Link>
-          ))}
-        </div>
+      <BookOverview {...sampleBooks[0]} />
+      <div className="flex flex-wrap justify-around px-10 w-full">
+        {sampleBooks.map((book) => (
+          <Link key={book.id} href={`/book/${book.id}`} passHref>
+            <BookCard {...book} />
+          </Link>
+        ))}
+      </div>
     </div>
-);
+  );
 }
-
---
+++ lib/mail.ts
@@ -0,0 +1,27 @@
+"use server";
+import * as nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+export default async function main(email: string, content: string) {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: "Library Management System", // sender address
+    to: email, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: content, // plain text body
+    html: content, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
