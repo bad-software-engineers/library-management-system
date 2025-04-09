d2f41559e61e9c5de03c46a4c3a4fac21885016a
MeetParmar2302 <meet.parmar.2302@gmail.com>
2025-04-09 12:24:41 +0530

    Admin dashboard side bar implementation attempt

---
+++ components/admin/Sidebar.tsx
@@ -0,0 +1,70 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarProvider,
+  } from "@/components/ui/sidebar"
+
+   const items = [
+    {
+      img: Home,
+      route: "/admin",
+      text: "Home",
+    },
+    {
+      img: Home,
+      route: "/admin/users",
+      text: "All Users",
+    },
+    {
+      img: Home,
+      route: "/admin/books",
+      text: "All Books",
+    },
+    {
+      img: Home,
+      route: "/admin/book-requests",
+      text: "Borrow Requests",
+    },
+    {
+      img: Home,
+      route: "/admin/account-requests",
+      text: "Account Requests",
+    },
+  ];
+
+const SideBar = () => {
+  return (
+    <SidebarProvider>
+        <Sidebar>
+        <SidebarContent>
+            <SidebarGroup>
+            <SidebarGroupLabel>BookWise</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                {items.map((item) => (
+                    <SidebarMenuItem key={item.text}>
+                    <SidebarMenuButton asChild>
+                        <a href={item.route}>
+                        <item.img />
+                        <span>{item.text}</span>
+                        </a>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+        </Sidebar>
+    </SidebarProvider>
+  )
+}
+
+export default SideBar
