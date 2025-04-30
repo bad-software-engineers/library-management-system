c746252c50696b2a0c72d12ef9202fcd6a6c5998
darshan2710 <darshandixit.dd27@gmail.com>
2025-04-30 11:45:53 +0530

    User Dashboard Transactions

--- components/admin/Header.tsx
+++ components/admin/Header.tsx
@@ -1,47 +1,47 @@
-import {  currentUser  } from '@clerk/nextjs/server'
-import {ClerkProvider, UserButton} from "@clerk/nextjs";
+import { currentUser } from '@clerk/nextjs/server'
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 import Link from 'next/link';
 
 const Header = async () => {
-    const user = await currentUser()
+  const user = await currentUser()
 
-    if (!user) return <div>Not signed in</div>
+  if (!user) return <div>Not signed in</div>
 
-    // const primaryEmailAddress = user.emailAddresses.filter((email) => email.id === user.primaryEmailAddressId);
+  // const primaryEmailAddress = user.emailAddresses.filter((email) => email.id === user.primaryEmailAddressId);
 
   return (
-      <ClerkProvider
-          appearance={{
-              variables: {
+    <ClerkProvider
+      appearance={{
+        variables: {
 
-              },
-          }}
-      >
+        },
+      }}
+    >
 
-    <header className="flex w-full bg-white justify-between px-5 rounded-xl">
+      <header className="flex w-full bg-white justify-between px-5 rounded-xl">
         <div className=" py-2 px-4">
-            <h2 className="text-2xl font-bold text-grey-400 ">
-                Welcome, {user.fullName}
-            </h2>
-            {/*<p>{primaryEmailAddress[0].emailAddress}</p>*/}
-            <p className="text-xl text-grey-400">
-                Monitor all of your users and books here
-            </p>
+          <h2 className="text-2xl font-bold text-grey-400 ">
+            Welcome, {user.fullName}
+          </h2>
+          {/*<p>{primaryEmailAddress[0].emailAddress}</p>*/}
+          <p className="text-xl text-grey-400">
+            Monitor all of your users and books here
+          </p>
         </div>
         <div className="flex items-center justify-center gap-5 pr-4">
           <Link href="/">
             <button className="bg-blue-600 text-white px-4 py-2 rounded-lg text-sm hover:bg-blue-700 transition">
-              Go to User Dashboard
+              Go to Home
             </button>
           </Link>
 
           <div className="scale-125 flex items-center justify-center">
-            <UserButton/>
+            <UserButton />
           </div>
         </div>
 
-    </header>
-      </ClerkProvider>
+      </header>
+    </ClerkProvider>
 
   )
 }
